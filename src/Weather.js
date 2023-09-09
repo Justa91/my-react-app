cf22a71ad3c5c84a5a3f52d5afd38c64198a35b5
Justyna <jkkrupaaa@gmail.com>
2023-09-09 19:54:13 +0200

    added real data

--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  return (
-    <div className="container">
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-7">
-              <input
-                type="search"
-                placeholder="Type a city..."
-                className="p-2 search-input"
-              ></input>
-            </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-            <div className="col-5 ps-0">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary submit-button"
-              />
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: "9 September",
+      city: response.data.city,
+      temperature: Math.round(response.data.temperature.current),
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
+    });
+
+    console.log(response.data);
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="Weather">
+          <form>
+            <div className="row">
+              <div className="col-7">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="p-2 search-input"
+                ></input>
+              </div>
+
+              <div className="col-5 ps-0">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary submit-button"
+                />
+              </div>
             </div>
-          </div>
-        </form>
-        <hr></hr>
-        <div className="weather-info mt-4">
-          <div className="row">
-            <div className="col-6">
-              <h2>Warsaw</h2>
-              <p>Friday 13:51</p>
-              <ul>
-                <li>
-                  <span>Humidity:</span> 50%
-                </li>
-                <li>
-                  <span>Wind:</span> 1km/h
-                </li>
-              </ul>
-            </div>
-            <div className="col-6 d-flex justify-content-end">
-              <span>ICON</span>
-              <span>13℃</span>
-              <span>few clouds</span>
+          </form>
+          <hr></hr>
+          <div className="weather-info mt-4">
+            <div className="row">
+              <div className="col-6">
+                <h2>{weatherData.city}</h2>
+                <p>{weatherData.date}</p>
+                <ul>
+                  <li>
+                    <span>Humidity:</span> {weatherData.humidity}%
+                  </li>
+                  <li>
+                    <span>Wind: </span>
+                    {weatherData.wind}km/h
+                  </li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <span>
+                  <img src={weatherData.icon} alt={weatherData.description} />
+                </span>
+                <span className="temperature-info">
+                  {weatherData.temperature}℃
+                </span>
+                <span>{weatherData.description}</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1t46b45ad73356d1fdb57b7o025ca415";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
